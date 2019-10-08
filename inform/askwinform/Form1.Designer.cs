4d2c2e54be734506def83d69641eaebe20ec24a8
Bellalau93 <496688859@qq.com>
2019-10-08 20:01:27 +0800

    Add files via upload

---
+++ inform/askwinform/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace askwinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pdinfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.hospitalDataSet = new askwinform.hospitalDataSet();
+            this.pd_infoTableAdapter = new askwinform.hospitalDataSetTableAdapters.pd_infoTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pdinfoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pdinfoBindingSource
+            // 
+            this.pdinfoBindingSource.DataMember = "pd_info";
+            this.pdinfoBindingSource.DataSource = this.hospitalDataSet;
+            // 
+            // hospitalDataSet
+            // 
+            this.hospitalDataSet.DataSetName = "hospitalDataSet";
+            this.hospitalDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // pd_infoTableAdapter
+            // 
+            this.pd_infoTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 97);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(820, 303);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(876, 478);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "叫号";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pdinfoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hospitalDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private hospitalDataSet hospitalDataSet;
+        private System.Windows.Forms.BindingSource pdinfoBindingSource;
+        private hospitalDataSetTableAdapters.pd_infoTableAdapter pd_infoTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ inform/askwinform/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace askwinform
+{
+    public partial class Form1 : Form
+    {
+        private DataGridView view1 = new DataGridView();
+        private BindingSource binding1 = new BindingSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GetData(string selectCommand)
+        {
+            SqlConnection mycon = new SqlConnection("Data Source=laptop-cl08flf2;Initial Catalog=hospital;Integrated Security=SSPI");
+            SqlDataAdapter myada = new SqlDataAdapter(selectCommand, mycon);
+            SqlCommandBuilder combui = new SqlCommandBuilder(myada);
+            DataTable table = new DataTable();
+            table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+            myada.Fill(table);
+            binding1.DataSource = table;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          dataGridView1.DataSource = binding1;
+            GetData("with cte as( select distinct patient_info.name, pd_info.number, pd_info.category, pd_info.expe, going from patient_info, doctor_info, pd_info where differ = 'true' and pd_info.phone = patient_info.phone ) select name as '姓名',number as '编号',category as '科室',expe as '门诊类别',going as '当前状态' from cte order by number");
+
+        }
+
+      
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = binding1;
+            GetData("with cte as( select distinct patient_info.name, pd_info.number, pd_info.category, pd_info.expe, going, pd_info.address from patient_info, doctor_info, pd_info where differ = 'true' and pd_info.phone = patient_info.phone) select name as '姓名',number as '编号',category as '科室',expe as '门诊类别',address as '诊室',going as '当前状态' from cte order by number");
+
+        }
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
